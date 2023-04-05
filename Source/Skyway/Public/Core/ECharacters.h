f4ad69355a7a8473ea7bb6c5b2e2074cfe0422fc
wizard0152 <aslan.mambetov.aslan@gmail.com>
2023-04-05 23:11:52 +0300

    save

--- Source/Skyway/Public/Core/ECharacters.h
+++
@@ -1,9 +0,0 @@
-
-
-UENUM(BlueprintType)
-enum class ECharacters : uint8
-{
-    Bot UMETA(DisplayName = "Bot"),
-    Bouncer UMETA(DisplayName = "Bouncer"),
-    Slime UMETA(DisplayName = "Slime"),
-};
